a88fedf5a37515db1942f0f56ec9bed124ad8d04
Your Name <you@example.com>
2020-12-14 23:06:42 +0200

    lesson1

---
+++ lesson-1/hw2.py
@@ -0,0 +1,13 @@
+
+a = input("Please Enter coefficient 'a': ")
+a = int(a)
+b = input("Please Enter coefficient 'b': ")
+b = int(b)
+c = input("Please Enter coefficient 'c': ")
+c = int(c)
+D = b * 2 - 4* a * c
+x1 = (-b + D * 0.5) / 2 * a
+x2 = (-b - D * 0.5) / 2 * a
+
+print(x1)
+print(x2)
